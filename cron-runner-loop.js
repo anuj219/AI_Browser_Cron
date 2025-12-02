7b2ca9b4a767f2a7262644636bc0f25004426689
pynuz <anujvmehta99@gmail.com>
2025-12-02 14:45:25 +0530

    loop added

---
+++ cron-runner-loop.js
@@ -0,0 +1,18 @@
+const { processWorkflows } = require('./workflows');
+require('dotenv').config();
+
+async function loop() {
+  while (true) {
+    console.log("🔄 Running workflow cycle...");
+    try {
+      await processWorkflows();
+    } catch (err) {
+      console.error("Cron error:", err);
+    }
+
+    console.log("⏳ Sleeping for 15 minutes...");
+    await new Promise(res => setTimeout(res, 15 * 60 * 1000));
+  }
+}
+
+loop();
